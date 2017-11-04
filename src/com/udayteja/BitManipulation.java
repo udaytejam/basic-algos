7a25881423c04f446a8dd7d9a9abd7cc09019c10
uday <udayteja23@gmail.com>
2017-11-05 01:26:54 +0530

    Updated bit manipulation definitions and analysis.

--- src/com/udayteja/BitManipulation.java
+++ src/com/udayteja/BitManipulation.java
@@ -14,12 +14,44 @@ public class BitManipulation {
         If input is 00110001(49), output is 11001110 (-50) because 0110001 + 1 = 50;
         */
 
-        //SIGNED LEFT SHIFT(<<)*******************************************************
-        byte x1 = 23, output;
+        //SIGNED LEFT SHIFT(<<)************************************************************
+        //It doubles the number with each shift
+        byte x1 = 23, x2 = -18;
         for (byte i = 0; i < 4; i++) {
-            //Java by default returns an integer for bitshift.
-            System.out.println("Left shifting " + x1 + " by " + i + " bit :" + (x1 << i));
+            //Java by default returns an integer for bit shift
+            System.out.println("Left shifting " + x1 + " by " + i + " bits :" + (x1 << i));
         }
+
         System.out.println("Left Bit shifting the maximum Integer value: " + Integer.MAX_VALUE + " becomes " + (Integer.MAX_VALUE << 1));
+
+        for (byte i = 0; i < 4; i++) {
+            //Java by default returns an integer for bit shift
+            System.out.println("Left shifting " + x2 + " by " + i + " bits :" + (x2 << i));
+        }
+
+        //SIGNED RIGHT SHIFT(>>)**********************************************************
+        //It halves the number with each shift
+        for (byte i = 0; i < 8; i++) {
+            //Java by default returns an integer for bit shift
+            System.out.println("Right shifting " + x1 + " by " + i + " bit :" + (x1 >> i));
+        }
+        //Let's Right Shift a negative number. Same halving happens, but 1 get's added if it is odd(Rounded up)
+        for (byte i = 0; i < 8; i++) {
+            //Java by default returns an integer for bit shift
+            System.out.println("Right shifting " + x2 + " by " + i + " bit :" + (x2 >> i));
+        }
+
+        /*This is also called Logical shifting. Which may create negative numbers. You may not observe doubling/halving
+        when you are shifting Integer.MAX_VALUE because there's no condition for Sign bit to retain it's value. It will
+        be considered just as any normal bit. There's an another type of shifting called 'Arithmetic shifting' which will
+        retain the Sign Bit and shift other numbers.
+        */
+        //Arithmetic shifting
+
+        //UNSIGNED RIGHT SHIFT(>>>)*******************************************************
+        for (byte i = 0; i < 8; i++) {
+            //Java by default returns an integer for bit shift
+            System.out.println("Unsigned Right shifting " + x2 + " by " + i + " bit :" + (x2 >>> i));
+        }
     }
 }
